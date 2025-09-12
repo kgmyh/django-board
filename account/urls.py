2454c90d8b206228b82a138fd9cf5302b9683900
Kim Sunghwan <kgmyh@naver.com>
2025-09-12 11:11:38 +0900

    Allow GET-based logout for UX: add views.logout_get and route /account/logout to it; restore anchor link in nav

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.urls import reverse_lazy
 from . import views
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from . import forms
 
@@ -17,6 +17,7 @@ urlpatterns = [
                                     success_url='/'), name='join'),
     # path('login', views.UserLoginView.as_view(), name='login'), #로그인 
     path('login', LoginView.as_view(template_name='account/login_form.html', form_class=AuthenticationForm), name='login'),
-    path('logout', LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
+    # 경고: GET 로그아웃 허용 (디자인 요구 대응). 보안상 권장하지 않음.
+    path('logout', views.logout_get, name='logout'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 # account/views.py
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.views.decorators.http import require_GET
 
 from .forms import CustomUserCreationForm
 # 가입 View
@@ -16,3 +18,10 @@ class UserCreateView(CreateView):
 class UserLoginView(LoginView):
     template_name = 'account/login_form.html'
     form_class = AuthenticationForm
+
+
+# 비권장: GET 요청으로 로그아웃 처리 (디자인 요구사항 대응)
+@require_GET
+def logout_get(request):
+    logout(request)
+    return redirect('home')
