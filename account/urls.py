28e92ee131cac8d93474b9e4b2befd441182a8c1
Kim Sunghwan <kgmyh@naver.com>
2025-09-12 11:07:02 +0900

    Redirect logout to home using LogoutView.next_page=reverse_lazy('home')

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 # account/urls.py
 
 from django.urls import path
+from django.urls import reverse_lazy
 from . import views
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
@@ -16,6 +17,6 @@ urlpatterns = [
                                     success_url='/'), name='join'),
     # path('login', views.UserLoginView.as_view(), name='login'), #로그인 
     path('login', LoginView.as_view(template_name='account/login_form.html', form_class=AuthenticationForm), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
+    path('logout', LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
 
 ]
