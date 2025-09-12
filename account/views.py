658a6259a7fd6f9acfd0656df07ccecea68c8966
Kim Sunghwan <kgmyh@naver.com>
2025-09-12 11:17:04 +0900

    Revert to POST logout and align nav item visually:\n- Use LogoutView (POST-only) again\n- Replace logout link with inline POST form styled as nav-link (no misalignment)\n- Remove temporary GET logout view

--- account/views.py
+++ account/views.py
@@ -4,8 +4,6 @@ from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect
-from django.contrib.auth import logout
-from django.views.decorators.http import require_GET
 
 from .forms import CustomUserCreationForm
 # 가입 View
@@ -19,9 +17,4 @@ class UserLoginView(LoginView):
     template_name = 'account/login_form.html'
     form_class = AuthenticationForm
 
-
-# 비권장: GET 요청으로 로그아웃 처리 (디자인 요구사항 대응)
-@require_GET
-def logout_get(request):
-    logout(request)
-    return redirect('home')
+    
